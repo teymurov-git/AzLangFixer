43069912d66b2bd0d890e54462f9020213d7678c
teymurov-git <teymurluyusif555@gmail.com>
2025-05-18 11:26:59 +0400

    updated

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
-from app.api.endpoints import telegram
+from aiogram import Bot
+from app.config.settings import settings
 
 app = FastAPI(title="Azərbaycan Dili Botu")
 
-app.include_router(telegram.router, prefix="/api")
+@app.on_event("startup")
+async def on_startup():
+    bot = Bot(token=settings.TELEGRAM_BOT_TOKEN)
+    webhook_url = "https://127.0.0.1:8000/api/webhook"
+    await bot.set_webhook(webhook_url)
+    print(f"Webhook quruldu: {webhook_url}")
 
 @app.get("/")
 async def root():
